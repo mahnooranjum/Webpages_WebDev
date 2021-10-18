f5f7c0ed57dda2f12843860fb4d3cdd71820c51e
Mahnoor Anjum <manomaq@gmail.com>
2021-10-18 22:49:11 +0500

    added stringify

--- JS_LearnersPath/4_DOM_pig_game/starter_arrays/script.js
+++ JS_LearnersPath/4_DOM_pig_game/starter_arrays/script.js
@@ -1,1 +1,35 @@
-alert("This works");
+
+const persons = [
+    { name: 'mahnoor', status: 'sleeping'}, 
+    { name: 'usman', status: 'working wajahat'}, 
+    { name: 'umer', status: 'dota'}, 
+    { name: 'abdullah', status: 'skype'}, 
+    { name: 'joker', status: 'serious'}, 
+    { name: 'adele', status: 'on the other side'}, 
+]
+
+
+const filteredPersons = persons.filter((person) => {
+    return person.status === 'sleeping';
+})
+
+const personNames = persons.map((person) => {
+    return person.name
+})
+
+// console.log(persons);
+// console.log(filteredPersons);
+// console.log(personNames);
+
+const body = document.body;
+div = document.createElement("div");
+div.innerHTML = JSON.stringify(persons, null, 2);
+document.body.append(div);
+
+div = document.createElement("div");
+div.innerHTML = JSON.stringify(filteredPersons, null, 2);
+document.body.append(div);
+
+div = document.createElement("div");
+div.innerHTML = personNames;
+document.body.append(div);
