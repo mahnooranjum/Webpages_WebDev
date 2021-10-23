08ff3c50476f53142464296245b7cc580ee5552a
Mahnoor Anjum <manomaq@gmail.com>
2021-10-23 17:39:15 +0500

    added object traversal

--- JS_LearnersPath/5_advanced_JS/starter_objectsAndClasses/script.js
+++ JS_LearnersPath/5_advanced_JS/starter_objectsAndClasses/script.js
@@ -1,1 +1,18 @@
-alert("This works");
+var Data = function(name, values){
+    this.name = name;
+    this.values = values;
+}
+
+
+var dataset1 = new Data('dataset1', {
+    x: [1,2,3],
+    y: [4,5,6]
+} );
+
+console.log(dataset1.values)
+for (const [key, value] of Object.entries(dataset1.values)) {
+    e = document.createElement('li');
+    e.className = 'list-group-item';
+    e.innerHTML = key + ':' + value;
+    document.querySelector('.list-group').append(e);
+  }
