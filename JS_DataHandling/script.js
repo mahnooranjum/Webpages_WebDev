7ee30d398362219c42a08bd8b4491c89c964c937
Mahnoor Anjum <manomaq@gmail.com>
2021-10-24 16:19:13 +0500

    added await async

--- JS_DataHandling/script.js
+++ JS_DataHandling/script.js
@@ -8,14 +8,23 @@
 // })
 
 
-console.log('about to fetch')
-fetch('data/sample.jpg').then(response => {
-    console.log(response);
-    return response.blob();
-}).then(blob => {
-    console.log(blob);
-    document.getElementById('sample').src = URL.createObjectURL(blob);
-}).catch(error =>{
-    console.log(error);
-})
+// console.log('about to fetch')
+// fetch('data/sample.jpg').then(response => {
+//     console.log(response);
+//     return response.blob();
+// }).then(blob => {
+//     console.log(blob);
+//     document.getElementById('sample').src = URL.createObjectURL(blob);
+// }).catch(error =>{
+//     console.log(error);
+// })
 
+drawImage().catch(error =>{
+                  console.log(error);
+                  })
+
+async function drawImage(){
+    const response = await fetch('data/sample.jpg');
+    const blob = await response.blob();
+    document.getElementById('sample').src = URL.createObjectURL(blob);
+}
