cd377273fc59fbb0108a89794d7cef5b30c1c43e
Mahnoor Anjum <manomaq@gmail.com>
2021-10-24 16:12:38 +0500

    added fetch for image

---
+++ JS_DataHandling/script.js
@@ -0,0 +1,8 @@
+console.log('about to fetch')
+fetch('data/sample.jpg').then(response => {
+    console.log(response);
+    return response.blob();
+}).then(blob => {
+    console.log(blob);
+    document.getElementById('sample').src = URL.createObjectURL(blob);
+})
