86225221264ff9a744326adc617504cd49e37db5
Mahnoor Anjum <manomaq@gmail.com>
2021-10-25 15:13:17 +0500

    added getJSON

--- JS_DataHandling/script.js
+++ JS_DataHandling/script.js
@@ -1,74 +1,14 @@
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    if (color !== '000000'){
-        return  '#' + color;
-    }else {
-        return getRandomColor()
-    }
+async function getJSON(){
 
-  }
-  
+    const data1 = await fetch('https://api.wheretheiss.at/v1/satellites');
+    const data1JSON = await data1.json();
+    console.log(data1JSON);
 
+    const data2 = await fetch('https://api.wheretheiss.at/v1/satellites/' + data1JSON[0].id)
+    const data2JSON = await data2.json();
+    console.log(data2JSON);
 
-// getData().then(()=>{
-//     getChart()
-// })
-
-async function getData(){
-    xs = []
-    ys = []
-
-      const response = await fetch('data/train_house.csv');
-      const data = await response.text();
-      // document.getElementById('sample-text').innerHTML = data;
-      var rows = data.split('\n').slice(1);
-      rows.forEach( row => {
-          col = row.split(',')  
-          console.log(col[1] + ', '+ col[3])
-          xs.push(col[1])
-          ys.push(col[3])
-          
-      })
-      return {xs, ys}
-  }
-
-
-
-async function getChart() {
-    const dataset = await getData();
-    var legends = ['My First Dataset'];
-    const color =  getRandomColor();
-    // console.log(colors)
-    // console.log(strokeColors)
-    
-    const data = {
-        labels: dataset.xs,
-        datasets: [{
-            label: legends,
-            backgroundColor: color,
-            borderWidth: 1,
-            data: dataset.ys,
-            minBarLength: 3,
-        }]
-    };
-    
-    typeGraph = 'scatter';
-    const config = {
-        type: typeGraph,
-        data: data,
-        options: {}
-      };
-    
-    var myChart = new Chart(
-        document.getElementById('sample-chart'),
-        config
-      );
-    
-    
 }
 
-getChart()
+
+getJSON()
