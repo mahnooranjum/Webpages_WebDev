9eecf0a0557048e6bc89f2495da2d9fa78ea29a4
Mahnoor Anjum <manomaq@gmail.com>
2021-10-27 09:24:46 +0500

    added scripts for json

--- JS_DataHandling/script.js
+++ JS_DataHandling/script.js
@@ -1,14 +1,14 @@
 async function getJSON(){
 
-    const data1 = await fetch('https://api.wheretheiss.at/v1/satellites');
-    const data1JSON = await data1.json();
-    console.log(data1JSON);
 
-    const data2 = await fetch('https://api.wheretheiss.at/v1/satellites/' + data1JSON[0].id)
-    const data2JSON = await data2.json();
-    console.log(data2JSON);
+    
+    // Use flask_cors for cor wrapped responses
+
+    var data1 = await fetch('http://127.0.0.1:5000/');
+    var dataJSON = await data1.json();
+
+    console.log(dataJSON)
 
 }
-
 
 getJSON()
---
+++ JS_DataHandling/script_json.js
@@ -0,0 +1,15 @@
+
+async function getJSON(){
+
+    const data1 = await fetch('https://api.wheretheiss.at/v1/satellites');
+    const data1JSON = await data1.json();
+    console.log(data1JSON);
+
+    const data2 = await fetch('https://api.wheretheiss.at/v1/satellites/' + data1JSON[0].id)
+    const data2JSON = await data2.json();
+    console.log(data2JSON);
+
+}
+
+
+getJSON()
