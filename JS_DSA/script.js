dddc4c0372848f57af888ce08cb071cf50657bad
Mahnoor Anjum <manomaq@gmail.com>
2022-01-11 22:34:59 +0500

    updated

--- JS_DSA/script.js
+++ JS_DSA/script.js
@@ -2,20 +2,42 @@
 
 const arr = [2,5,99, 7,9,1,10,11,26,3,1]
 
-function insertionSort(arr) {
-    let n = arr.length;
-        for (let i = 1; i < n; i++) {
-            let current = arr[i];
-            let j = i-1; 
-            while ((j > -1) && (current < arr[j])) {
-                arr[j+1] = arr[j];
-                j--;
-            }
-            arr[j+1] = current;
-        }
-    return arr;
+function mergeSort(arr) {
+
+
+    const length = arr.length;
+    if(length === 1){
+        return arr
+    }
+
+    const m = Math.floor(length/2);
+    const l   = arr.slice(0, m)
+    const r   = arr.slice(m)
+
+    return merge(mergeSort(l), mergeSort(r))
+
+
 }
 
-insertionSort(arr)
 
-console.log(arr)
+function merge(l, r){
+    const result = []
+    let li = 0
+    let ri = 0
+
+    while (li < l.length && ri < r.length){
+        if (l[li] < r[ri]){
+            result.push(l[li])
+            li++
+        }else{
+            result.push(r[ri])
+            ri++
+        }
+    }
+
+    return result.concat(l.slice(li)).concat(r.slice(ri))
+}
+
+
+
+console.log(mergeSort(arr))
