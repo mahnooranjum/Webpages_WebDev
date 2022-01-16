cca16ebafa9317a0e956a49fbf02065e814e810e
Mahnoor Anjum <manomaq@gmail.com>
2022-01-16 22:20:06 +0500

    added files placeholder

--- JS_bigO/script.js
+++ JS_bigO/script.js
@@ -8,37 +8,31 @@ class Node{
 }
 
 
-const a = new Node(5)
-const b = new Node(11)
-const c = new Node(3)
-const d = new Node(4)
-const e = new Node(2)
-const f = new Node(1)
+const a = new Node(9)
+const b = new Node(4)
+const c = new Node(20)
+const d = new Node(1)
+const e = new Node(6)
+const f = new Node(15)
+const g = new Node(170)
 
 a.left = b
 a.right = c
 b.left = d
 b.right = e
-c.right = f
+c.left = f
+c.right = g
 
+// depth first 
 
-const treeSumMax = (root) => {
-    if (root === null){
-        return -1*Infinity
-    }
-    if (root.left === null && root.right === null){
-        
-        return root.val
-    } else if(root.left === null) {
-        return root.val + treeSumMax(root.left)
-    } else if(root.right === null){
-        return root.val + treeSumMax(root.right)
-    } else {
-        return Math.max(root.val + treeSumMax(root.left), root.val + treeSumMax(root.right) ) 
-    }
+const dfsPostOrder = (root) => {
+    if (root === null) return []
 
+    const leftvals = dfsPostOrder(root.left)
+    const rightvals = dfsPostOrder(root.right)
 
+    return [... leftvals,  ... rightvals, root.val]
 } 
 
 
-console.log(treeSumMax(a))
+console.log(dfsPostOrder(a))
