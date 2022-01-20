0fb66bb29e39beba8e94736440e0512a7ccc7f91
Mahnoor Anjum <manomaq@gmail.com>
2022-01-21 00:46:05 +0500

    added

--- JS_Recursion/script.js
+++ JS_Recursion/script.js
@@ -1,17 +1,39 @@
 
-string = 'kayak'
 
-const isPalindrome = (str) => {
-    if (str.length == 1 || str.length == 0){
-        return true 
+class Node{
+    constructor(value){
+        this.value= value;
+        this.next = null;
+    }
+}
+
+var head = new Node(10)
+head.next = new Node(11)
+head.next.next = new Node(12)
+head.next.next.next = new Node(13)
+head.next.next.next.next = new Node(14)
+head.next.next.next.next.next = null
+
+function reverse(node){
+    if (node == null || node.next == null){
+    return node
+    }
+    var p = reverse(node.next)
+    node.next.next = node
+    node.next = null
+    return p
+
+}
+
+head = reverse(head)
+
+function printer(head){
+    while (head != null){
+        console.log(head.value)
+        head = head.next
     }
 
-    if (str.charAt(0) == str.charAt(str.length - 1)){
-        return isPalindrome(str.slice(1, str.length - 1))
-    }
 
-    return false
-} 
+}
 
-
-console.log(isPalindrome(string))
+printer(head)
